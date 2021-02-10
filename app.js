53d68f26629c590166cec2ae42b3297c80329cfe
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-10 10:43:53 +0900

    implementing special attack

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ new Vue({
         },
         attack: function(){
             this.monsterHealth -= this.calculateDamage(3, 10);
-            if ( this.checkWin()){
+            if (this.checkWin()){
                 return;
             }
 
@@ -21,7 +21,11 @@ new Vue({
             this.checkWin();
         },
         specialAttack: function() {
-
+            this.monsterHealth -= this.calculateDamage(10, 20);
+            if (this.checkWin()){
+                return;
+            }
+            this.monsterAttacks();
         },
         heal: function() {
 
@@ -29,19 +33,23 @@ new Vue({
         giveUp: function(){
 
         },
+        monsterAttacks: function() {
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
         calculateDamage: function (min, max){
             return Math.max(Math.floor(Math.random()* max) + 1, min) 
         },
         checkWin: function(){
             if (this.monsterHealth <= 0){
-                if(confirm('You won! New Game>')){
+                if(confirm('You won! New Game?')){
                     this.startGame();
                 }else {
                     this.gameIsRunning = false;
                 }
                 return true;
             } else if (this.playerHealth <= 0){
-                if(confirm('You lost! New Game>')){
+                if(confirm('You lost! New Game?')){
                     this.startGame();
                 }else {
                     this.gameIsRunning = false;
