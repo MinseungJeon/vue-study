8486bb48a6d54e73726b1fd1184e94ab11ec796f
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-10 11:52:43 +0900

    using multiple vue instance

--- app.js
+++ app.js
@@ -1,78 +1,33 @@
 new Vue({
-    el: '#app',
-    data: {
-        playerHealth: 100,
-        monsterHealth: 100,
-        gameIsRunning: false,
-        turns: []
+  el: '#app1',
+  data: {
+    title: 'The VueJS Instance',
+    showParagraph: false
+  },
+  methods: {
+    show: function() {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)');
     },
-    methods: {
-        startGame: function() {
-            this.gameIsRunning = true;
-            this.playerHealth = 100;
-            this.monsterHealth = 100;
-        },
-        attack: function(){
-            let damage = this.calculateDamage(3, 10);
-            this.monsterHealth -= damage;
-            this.turns.unshift({
-                isPlayer: true,
-                text: 'Player hits Monster for ' + damage
-            });
-            if (this.checkWin()){
-                return;
-            }
-
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
-        },
-        specialAttack: function() {
-            this.monsterHealth -= this.calculateDamage(10, 20);
-            if (this.checkWin()){
-                return;
-            }
-            this.monsterAttacks();
-        },
-        heal: function() {
-            if(playerHealth <= 90){
-                this.playerHealth += 10;
-            } else {
-                this.playerHealth = 100;
-            }
-            this.monsterAttacks();
-        },
-        giveUp: function(){
-
-        },
-        monsterAttacks: function() {
-            let damage = this.calculateDamage(5, 12);
-            this.playerHealth -= damage;
-            this.checkWin();
-            this.turns.unshift({
-                isPlayer: false,
-                text: 'Monster hits Player for ' + damage
-            })
-        },
-        calculateDamage: function (min, max){
-            return Math.max(Math.floor(Math.random()* max) + 1, min) 
-        },
-        checkWin: function(){
-            if (this.monsterHealth <= 0){
-                if(confirm('You won! New Game?')){
-                    this.startGame();
-                }else {
-                    this.gameIsRunning = false;
-                }
-                return true;
-            } else if (this.playerHealth <= 0){
-                if(confirm('You lost! New Game?')){
-                    this.startGame();
-                }else {
-                    this.gameIsRunning = false;
-                }
-                return true;
-        }
-        return false;
+    updateTitle: function(title) {
+      this.title = title;
     }
+  },
+  computed: {
+    lowercaseTitle: function() {
+      return this.title.toLowerCase();
     }
+  },
+  watch: {
+    title: function(value) {
+      alert('Title changed, new value: ' + value);
+    }
+  }
+});
+
+new Vue({
+  el: '#app2',
+  data: {
+    title: 'The second Instance'
+  }
 })
